97f7f3f832c207c45d16aec8ef7f405cb9f001aa
kain <zhangtqx@vip.qq.com>
2019-06-21 18:15:38 +0800

    Submit

--- src/app/quick/quick.component.ts
+++ src/app/quick/quick.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-quick',
@@ -6,10 +6,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quick.component.scss']
 })
 export class QuickComponent implements OnInit {
+  step = 0;
+  index = 'First-content';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  pre(): void {
+    this.step -= 1;
+    this.changeContent();
+  }
+
+  next(): void {
+    this.step += 1;
+    this.changeContent();
+  }
+
+  done(): void {
+    console.log('done');
+  }
+
+  changeContent(): void {
+    switch (this.step) {
+      case 0: {
+        this.index = 'First-content';
+        break;
+      }
+      case 1: {
+        this.index = 'Second-content';
+        break;
+      }
+      case 2: {
+        this.index = 'third-content';
+        break;
+      }
+      default: {
+        this.index = 'error';
+      }
+    }
+
+  }
 }
