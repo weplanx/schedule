0fd596b3c600f5fdc09e6b4f897eba82530e2d26
kain <zhangtqx@vip.qq.com>
2019-06-21 16:07:07 +0800

    Submit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterModule, Routes} from '@angular/router';
+import {registerLocaleData} from '@angular/common';
+import {HttpClientModule} from '@angular/common/http';
+import zh from '@angular/common/locales/zh';
+import {NgZorroAntdModule, NZ_I18N, zh_CN} from 'ng-zorro-antd';
 
-import { AppComponent } from './app.component';
+registerLocaleData(zh);
+
+import {AppComponent} from './app.component';
+
+// const routes: Routes = [
+//   {path: '', loadChildren: './app.router.module#AppRouterModule', canActivate: [TokenService]},
+//   {path: 'login', loadChildren: './login/login.module#LoginModule'},
+// ];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NgZorroAntdModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    {provide: NZ_I18N, useValue: zh_CN}
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
