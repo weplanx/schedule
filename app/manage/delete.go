539485c0b20b684b6562b816ea892cc778cf1967
kain <zhangtqx@vip.qq.com>
2020-09-03 14:47:25 +0800

    update delete error

--- app/manage/delete.go
+++ app/manage/delete.go
@@ -1,6 +1,9 @@
 package manage
 
 func (c *JobsManager) Delete(identity string) (err error) {
+	if c.options.Empty(identity) {
+		return
+	}
 	c.termination(identity)
 	c.options.Clear(identity)
 	c.runtime.Clear(identity)
