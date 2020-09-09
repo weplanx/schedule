fa9627aa77dccb5ae05c7fdacc87284581d736a5
kain <zhangtqx@vip.qq.com>
2020-09-09 17:14:20 +0800

    update

--- app/app.go
+++ app/app.go
@@ -11,30 +11,20 @@ import (
 	pb "schedule-microservice/router"
 )
 
-type App struct {
-	option *types.Config
-}
-
-func New(config types.Config) *App {
-	app := new(App)
-	app.option = &config
-	return app
-}
-
-func (app *App) Start() (err error) {
+func Application(option *types.Config) (err error) {
 	// Turn on debugging
-	if app.option.Debug {
+	if option.Debug {
 		go func() {
 			http.ListenAndServe(":6060", nil)
 		}()
 	}
 	// Start microservice
-	listen, err := net.Listen("tcp", app.option.Listen)
+	listen, err := net.Listen("tcp", option.Listen)
 	if err != nil {
 		return
 	}
 	server := grpc.NewServer()
-	manager, err := manage.NewJobsManager(&app.option.Logging)
+	manager, err := manage.NewJobsManager(&option.Logging)
 	if err != nil {
 		return
 	}
