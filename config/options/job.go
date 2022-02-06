44c1ee09456959c49499c3272fc817bca1761753
kain <zhangtqx@vip.qq.com>
2022-02-06 14:23:08 +0800

    update

--- config/options/job.go
+++
@@ -1,8 +0,0 @@
-package options
-
-type JobOption struct {
-	Identity string                  `yaml:"identity"`
-	TimeZone string                  `yaml:"time_zone"`
-	Start    bool                    `yaml:"start"`
-	Entries  map[string]*EntryOption `yaml:"entries"`
-}
--- config/options/transfer.go
+++
@@ -1,6 +0,0 @@
-package options
-
-type TransferOption struct {
-	Listen string `yaml:"listen"`
-	Pipe   string `yaml:"pipe"`
-}
